73923fda006724a9b74c2c0d92a5387691e73d0b
nsahloum <sahloumi.najima@gmail.com>
2020-09-11 23:54:40 +0200

    index.php

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,6 @@
 FROM debian:buster
 
-RUN apt-get update \
-&& apt-get upgrade -y \
-&& apt-get install nginx -y \
-&& apt-get install mariadb-server mariadb-client -y \
-&& apt-get install php -y \
-&& apt-get install wget -y
-
-RUN wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
-
-COPY src/ /var/www/html
+COPY srcs/index.php ./
+COPY srcs/nginx.conf ./
+COPY srcs/init.sh ./
+CMD bash init.sh && tail -f /dev/null
---
+++ srcs/init.sh
@@ -0,0 +1,16 @@
+apt-get update && apt-get install -y
+apt-get install -y wget
+apt-get install -y curl
+apt-get install -y nginx
+apt-get install -y php-fpm php-mysql
+apt-get install -y mariadb-server
+apt-get install -y php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
+chown -R www-data:www-data /var/www/html
+service nginx start
+cp nginx.conf /etc/nginx/sites-available/nginx.conf
+ln -s /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/
+rm /etc/nginx/sites-enabled/default
+mkdir /var/www/php
+cp index.php /var/www/php
+service php7.3-fpm start
+service nginx restart
