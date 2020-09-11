e45419a78a0d0f8f4c9f020105e4f66b94057b21
nsahloum <sahloumi.najima@gmail.com>
2020-09-12 00:06:45 +0200

    ln -s more logical

--- srcs/init.sh
+++ srcs/init.sh
@@ -8,7 +8,7 @@ apt-get install -y php-curl php-gd php-intl php-mbstring php-soap php-xml php-xm
 chown -R www-data:www-data /var/www/html
 service nginx start
 cp nginx.conf /etc/nginx/sites-available/nginx.conf
-cp /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/
 mkdir /var/www/php
 cp index.php /var/www/php
 service php7.3-fpm start
